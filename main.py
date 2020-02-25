2f68ec8c8360f6cedbf8b6fb84bef1fac90ca481
idelfrides <idelfridesjorgepapai@gmail.com>
2020-02-25 16:35:55 -0300

    The real changes I need

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main_method():
         "value4": 3
     }
 
-    user_values = {}
+    user_values = ()
+    print("\n\n Enter some values separeted by commer\n")
     try:
         user_values = input("\n\n Enter some values:  ")
     except Exception as exec_error:
@@ -22,14 +23,20 @@ def main_method():
     else:
         # print(type(user_values), '\n Value: ', user_values)
         user_values = user_values.strip().split(',')
-        print(user_values)
 
     result = ml.sum_dict(my_values)
-    print(result)
+    print("\n\n MY VALUES: {}".format(result))
 
+    user_values_help = list(user_values)
+    user_values.clear()
+    for value in user_values_help:
+        user_values.append(int(value))   
+    
+    result = ml.sum_list(user_values)
+    print("\n\n USER VALUES: {}".format(result))
+
+    print("\n\n\n")
 
 
 if __name__=='__main__':
     main_method()
-
-
