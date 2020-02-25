c1d8473aa4efeb5e0bfe8ecb3fda61f99b1aaac9
idelfrides <idelfridesjorgepapai@gmail.com>
2020-02-25 15:46:20 -0300

    Sum of a list of value seted up

--- libs/math_lib.py
+++ libs/math_lib.py
@@ -6,6 +6,8 @@
 class MathLib(object):
 
     def sum_dict(self, dict_values):
+        """ Sum a dict of values """
+
         total_sum = 0
         if dict_values:
             for key in dict_values.keys():
@@ -17,6 +19,8 @@ class MathLib(object):
 
 
     def sum_list(self, list_values):
+        """ Sum a list of values """
+        
         total_sum = 0
         for value in list_values:
             total_sum += value
