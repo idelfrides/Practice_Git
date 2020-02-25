8e720be8cf89d766ccfb5a3da1b0a3775d1e3cf1
idelfrides <idelfridesjorgepapai@gmail.com>
2020-02-25 15:57:39 -0300

    Sum of a list of value seted up again

--- libs/math_lib.py
+++ libs/math_lib.py
@@ -6,6 +6,7 @@
 class MathLib(object):
 
     def sum_dict(self, dict_values):
+        """ Sum of a dict of values """
         total_sum = 0
         if dict_values:
             for key in dict_values.keys():
@@ -17,39 +18,9 @@ class MathLib(object):
 
 
     def sum_list(self, list_values):
+        """ Sum of a list of values """
         total_sum = 0
         for value in list_values:
             total_sum += value
 
         return total_sum
-
-
-
-
-
-    '''
-    
-    def sum_method(self, _values):
-        
-        def sum_list(self, list_values):
-            pass
-
-
-        if type(_values) == list:
-            return self.sum_list(_values)
-
-        if type(_values) == dict:
-            return self.sum_dict(_values)
-
-
-        def sum_dict(self, dict_values):
-            total_sum = 0
-            if dict_values:
-                for key in dict_values.keys():
-                    total_sum += dict_values[key]
-            else:
-                print("\n\n INVALID DICTIONARY")
-        
-            return total_sum'''
-
-
--- main.py
+++ main.py
@@ -13,30 +13,32 @@ def main_method():
         "value4": 3
     }
 
-    user_values = {}
+    user_values = ()
+    print("\n\n Enter some values separeted by commer\n")
     try:
         user_values = input("\n\n Enter some values:  ")
     except Exception as exec_error:
         print(exec_error)
         exit(0)
     else:
+        # print(type(user_values), '\n Value: ', user_values)
+        user_values = user_values.strip().split(',')
+        # import pdb; pdb.set_trace()
 
-        print(user_values)
-        # user_values = list(user_values)
-        # user_values = user_values.split(',')
-
-    # print(user_values)
     result = ml.sum_dict(my_values)
+    print("\n\n MY VALUES: {}".format(result))
 
-    # result = ml.sum_method(user_values)
+    user_values_help = list(user_values)
+    user_values.clear()
+    for value in user_values_help:
+        user_values.append(int(value))   
+    
+    result = ml.sum_list(user_values)
+    print("\n\n USER VALUES: {}".format(result))
 
-    print(result)
-
-
+    print("\n\n\n")
 
 
 
 if __name__=='__main__':
     main_method()
-
-
